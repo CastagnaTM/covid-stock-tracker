b8ed94e6b53433dd6c2f49ba1d0642dfe157eb0a
Ting Ting Ye <tingtingye24@gmail.com>
2020-05-26 12:32:40 -0400

    tested unix converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import {finnhubKey, finnhubBase} from './constants';
+import React from "react";
+import { finnhubKey, finnhubBase } from "./constants";
 
-
-const getStockData = ():any => {
-    fetch(`${finnhubBase}stock/candle?symbol=AAPL&resolution=D&from=1577750400&to=1590510123&token=${finnhubKey}`) 
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);        
+const getStockData = (): any => {
+  fetch(
+    `${finnhubBase}stock/candle?symbol=AAPL&resolution=D&from=1577750400&to=1590510123&token=${finnhubKey}`
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
     });
+};
 
-  }
+
+const convertToRealTime = (unixTimestamp: number): string => {
+    let milliseconds = unixTimestamp * 1000 // 1575909015000
+    let dateObject = new Date(milliseconds)
+    let humanDateFormat = dateObject.toLocaleString()
+    return humanDateFormat
+} 
 
 const App: React.FC = () => {
-    return (
-        <div>
-            {getStockData()}
-        </div>
-    );
-}
+return <div>{getStockData()}<div>{convertToRealTime(10)}</div></div>;
+};
 
 export default App;
