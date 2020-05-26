3c55ecea7b3c59e1268071c285b4014b53f19451
Cees Wang <wangcees@gmail.com>
2020-05-26 12:19:18 -0400

    fixed getstock function

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,21 @@ import React from 'react';
 import {finnhubKey, finnhubBase} from './constants';
 
 
-const getStockData = (): any => {
-    console.log(finnhubBase)
-    console.log(finnhubKey)
-    return fetch(`${finnhubBase}stock/candle?symbol=AAPL&resolution=D&from=1572651390&to=1573776000&token=${finnhubKey}`) 
-    .then(resp => {
-        console.log("hello")
-        return resp.json()
-    }
-        )
+const getStockData = ():any => {
+    fetch(`${finnhubBase}stock/candle?symbol=AAPL&resolution=D&from=1572651390&to=1573776000&token=${finnhubKey}`) 
+    .then(resp => resp.json())
     .then(data => {
-        console.log(data);  
-        return data;
-      
+        console.log(data);        
     });
 
   }
 
-// function getStockData<Object>(): Promise<Object> {
-//     return fetch(`${finnhubBase}stock/candle?symbol=AAPL&resolution=D&from=1572651390&to=1573776000&token=br2kub7rh5rbm8ou45ng`)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error(response.statusText)
-//         }
-//         return response.json()
-//       })
-  
-//   }
-
 const App: React.FC = () => {
-    return <div>{getStockData().toString()}</div>
+    return (
+        <div>
+            {getStockData()}
+        </div>
+    );
 }
 
 export default App;
