64d0020f4d3c8ff55c052d5f9dd4789c440c799a
Ting Ting Ye <tingtingye24@gmail.com>
2020-06-05 14:34:40 -0400

    got the lines to show up need to figure out the x axis and the zoom in

--- src/Components/ZoomGraph.js
+++ src/Components/ZoomGraph.js
@@ -6,28 +6,29 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
   
    componentDidMount = () => {
        this.setState({
-        data: [
-          { name: 1, cost: 4.11, impression: 100 },
-          { name: 2, cost: 2.39, impression: 120 },
-          { name: 3, cost: 1.37, impression: 150 },
-          { name: 4, cost: 1.16, impression: 180 },
-          { name: 5, cost: 2.29, impression: 200 },
-          { name: 6, cost: 3, impression: 499 },
-          { name: 7, cost: 0.53, impression: 50 },
-          { name: 8, cost: 2.52, impression: 100 },
-          { name: 9, cost: 1.79, impression: 200 },
-          { name: 10, cost: 2.94, impression: 222},
-          { name: 11, cost: 4.3, impression: 210 },
-          { name: 12, cost: 4.41, impression: 300 },
-          { name: 13, cost: 2.1, impression: 50 },
-          { name: 14, cost: 8, impression: 190 },
-          { name: 15, cost: 0, impression: 300 },
-          { name: 16, cost: 9, impression: 400 },
-          { name: 17, cost: 3, impression: 200 },
-          { name: 18, cost: 2, impression: 50 },
-          { name: 19, cost: 3, impression: 100 },
-          { name: 20, cost: 7, impression: 100 }
-        ],
+        // data: [
+        //   { name: 1, cost: 4.11, impression: 100 },
+        //   { name: 2, cost: 2.39, impression: 120 },
+        //   { name: 3, cost: 1.37, impression: 150 },
+        //   { name: 4, cost: 1.16, impression: 180 },
+        //   { name: 5, cost: 2.29, impression: 200 },
+        //   { name: 6, cost: 3, impression: 499 },
+        //   { name: 7, cost: 0.53, impression: 50 },
+        //   { name: 8, cost: 2.52, impression: 100 },
+        //   { name: 9, cost: 1.79, impression: 200 },
+        //   { name: 10, cost: 2.94, impression: 222},
+        //   { name: 11, cost: 4.3, impression: 210 },
+        //   { name: 12, cost: 4.41, impression: 300 },
+        //   { name: 13, cost: 2.1, impression: 50 },
+        //   { name: 14, cost: 8, impression: 190 },
+        //   { name: 15, cost: 0, impression: 300 },
+        //   { name: 16, cost: 9, impression: 400 },
+        //   { name: 17, cost: 3, impression: 200 },
+        //   { name: 18, cost: 2, impression: 50 },
+        //   { name: 19, cost: 3, impression: 100 },
+        //   { name: 20, cost: 7, impression: 100 }
+        // ],
+        data: this.props.data,
         left : 'dataMin',
         right : 'dataMax',
         refAreaLeft : '',
@@ -39,6 +40,13 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
         animation : true
       })
       console.log("hello")
+   }
+   componentDidUpdate(prevProps){
+     if(this.state.data !== this.props.data){
+       this.setState({
+         data: this.props.data
+       })
+     }
    }
 
    getAxisYDomain = (from, to, ref, offset) => {
@@ -121,11 +129,12 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
                 onMouseUp = { this.zoom.bind( this ) }
               >
                 <CartesianGrid strokeDasharray="3 3"/>
+                {console.log(this.state.data)}
                 <XAxis 
-                  allowDataOverflow={true}
-                  dataKey="name"
-                  domain={[this.state.left, this.state.right]}
-                  type="number"
+                  // allowDataOverflow={true}
+                  dataKey="date"
+                  // domain={[this.state.left, this.state.right]}
+                  // type="String"
                 />
                 <YAxis 
                   allowDataOverflow={true}
@@ -133,16 +142,19 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
                   type="number"
                   yAxisId="1"
                  />
-                <YAxis 
+                {/* <YAxis 
                   orientation="right"
                   allowDataOverflow={true}
                   domain={[this.state.bottom2, this.state.top2]}
                   type="number"
                   yAxisId="2"
-                 /> 
+                 />  */}
                 <Tooltip/>
-                <Line yAxisId="1" type='natural' dataKey='cost' stroke='#8884d8' animationDuration={300}/>
-                <Line yAxisId="2" type='natural' dataKey='impression' stroke='#82ca9d' animationDuration={300}/>
+                <Line yAxisId="1" type='natural' dataKey='open_price' stroke='#8884d8' animationDuration={300}/>
+                <Line yAxisId="1" type='natural' dataKey='close_price' stroke='#82ca9d' animationDuration={300}/>   
+                <Line yAxisId="1" type='natural' dataKey='low_price' stroke='#82ca9d' animationDuration={300}/>   
+                <Line yAxisId="1" type='natural' dataKey='high_price' stroke='#82ca9d' animationDuration={300}/>       
+     
                 
                 {
                     (this.state.refAreaLeft && this.state.refAreaRight) ? (
