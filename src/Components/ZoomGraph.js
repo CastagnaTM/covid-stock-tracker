77fb8020d8e9b132601581be4ab267d611487590
castagnaTM <castagna.tm@gmail.com>
2020-06-05 14:11:32 -0400

    reverted chart to placeholder

--- src/Components/ZoomGraph.js
+++ src/Components/ZoomGraph.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceArea } from 'recharts';
+
   
   export default class StreamingDemo extends React.Component {
   
    componentDidMount = () => {
        this.setState({
-        // [
-        //   { name: 1, cost: 4.11, impression: 100 },
-        //   { name: 2, cost: 2.39, impression: 120 },
-        //   { name: 3, cost: 1.37, impression: 150 },
-        //   { name: 4, cost: 1.16, impression: 180 },
-        //   { name: 5, cost: 2.29, impression: 200 },
-        //   { name: 6, cost: 3, impression: 499 },
-        //   { name: 7, cost: 0.53, impression: 50 },
-        //   { name: 8, cost: 2.52, impression: 100 },
-        //   { name: 9, cost: 1.79, impression: 200 },
-        //   { name: 10, cost: 2.94, impression: 222},
-        //   { name: 11, cost: 4.3, impression: 210 },
-        //   { name: 12, cost: 4.41, impression: 300 },
-        //   { name: 13, cost: 2.1, impression: 50 },
-        //   { name: 14, cost: 8, impression: 190 },
-        //   { name: 15, cost: 0, impression: 300 },
-        //   { name: 16, cost: 9, impression: 400 },
-        //   { name: 17, cost: 3, impression: 200 },
-        //   { name: 18, cost: 2, impression: 50 },
-        //   { name: 19, cost: 3, impression: 100 },
-        //   { name: 20, cost: 7, impression: 100 }
-        // ]
-        
+        data: [
+          { name: 1, cost: 4.11, impression: 100 },
+          { name: 2, cost: 2.39, impression: 120 },
+          { name: 3, cost: 1.37, impression: 150 },
+          { name: 4, cost: 1.16, impression: 180 },
+          { name: 5, cost: 2.29, impression: 200 },
+          { name: 6, cost: 3, impression: 499 },
+          { name: 7, cost: 0.53, impression: 50 },
+          { name: 8, cost: 2.52, impression: 100 },
+          { name: 9, cost: 1.79, impression: 200 },
+          { name: 10, cost: 2.94, impression: 222},
+          { name: 11, cost: 4.3, impression: 210 },
+          { name: 12, cost: 4.41, impression: 300 },
+          { name: 13, cost: 2.1, impression: 50 },
+          { name: 14, cost: 8, impression: 190 },
+          { name: 15, cost: 0, impression: 300 },
+          { name: 16, cost: 9, impression: 400 },
+          { name: 17, cost: 3, impression: 200 },
+          { name: 18, cost: 2, impression: 50 },
+          { name: 19, cost: 3, impression: 100 },
+          { name: 20, cost: 7, impression: 100 }
+        ],
         left : 'dataMin',
         right : 'dataMax',
         refAreaLeft : '',
@@ -38,23 +38,11 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
         bottom2 : 'dataMin-20',
         animation : true
       })
+      console.log("hello")
    }
 
-  //  componentDidUpdate = (prevProps) => {
-  //    if(this.props.data !== prevProps.data){
-  //      this.setState({ 
-  //        data: this.props.data 
-  //      })
-  //    }
-  //   this.showData();
-  //  }
-
-  //  showData = () => {
-  //    console.log(this.state.data ? this.state.data : "no" ) 
-  //  }
-
    getAxisYDomain = (from, to, ref, offset) => {
-    const refData = this.props.data.slice(from-1, to);
+    const refData = this.state.data.slice(from-1, to);
     let [ bottom, top ] = [ refData[0][ref], refData[0][ref] ];
     refData.forEach( d => {
         if ( d[ref] > top ) top = d[ref];
@@ -85,7 +73,7 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
       this.setState( () => ({
         refAreaLeft : '',
         refAreaRight : '',
-        data : data.slice(),
+          data : data.slice(),
         left : refAreaLeft,
         right : refAreaRight,
         bottom, top, bottom2, top2
@@ -135,7 +123,7 @@ import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Reference
                 <CartesianGrid strokeDasharray="3 3"/>
                 <XAxis 
                   allowDataOverflow={true}
-                  dataKey="date"
+                  dataKey="name"
                   domain={[this.state.left, this.state.right]}
                   type="number"
                 />
