39e5933fbbe8e729d39a171ad535e5859ac4d726
Ting Ting Ye <tingtingye24@gmail.com>
2020-06-16 12:18:40 -0400

    removed moved company data as it's own component

--- src/Components/CompanyData.tsx
+++ src/Components/CompanyData.tsx
@@ -36,33 +36,4 @@ function CompanyData({companyData}: Props) {
 
 export default CompanyData;
 
-// import React from "react";
-// import { convertToRealTime } from "../functions";
-// import { significantDates } from "../constants";
-
-// interface Props {
-//   data: any;
-// }
-
-// function CovidDates({ data }: Props) {
-//   const filterDates = (data: any): any => {
-//     // console.log(data);
-//     // console.log(significantDates);
-//     let filteredDates = significantDates.filter((date) => {
-//       let unix = new Date(date.date).getTime() / 1000;
-//       if (data.length > 1) {
-//         if (unix <= data[data.length-1].date_number && unix >= data[0].date_number) {
-//             return date;
-//         }
-//       }
-      
-//     });
-//     console.log(filteredDates)
-//     return filterDates
-//   };
-
-//   return <div>{filterDates(data)}</div>;
-// }
-
-// export default CovidDates;
 
