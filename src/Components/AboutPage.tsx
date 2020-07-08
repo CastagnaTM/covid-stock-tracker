5746e87cd2037c98ec581def1fb5f1ecc1885d29
castagnaTM <castagna.tm@gmail.com>
2020-07-08 11:36:47 -0400

    updates to about page

--- src/Components/AboutPage.tsx
+++ src/Components/AboutPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Cees from './headshot.png';
 
 function AboutPage() {
   return (
     <div>
       <p>Hello this is the about page and will soon contain a headshot of Cees :D</p>
+      <img src={Cees}
+        style={{marginLeft: '20px'}}
+      ></img>
       
     </div>
   );
