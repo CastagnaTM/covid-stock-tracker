2cf9661976d3436fc539ccb9491e075b7fd250af
castagnaTM <castagna.tm@gmail.com>
2020-07-21 12:50:34 -0400

    Updates

---
+++ precache-manifest.8332fc1f852004e8fb321ee97571848f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50ff338edc66133a7329428127cf78f",
+    "url": "/covid-stock-tracker/index.html"
+  },
+  {
+    "revision": "b7e7132ce985cb0409ad",
+    "url": "/covid-stock-tracker/static/js/2.71eb3844.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid-stock-tracker/static/js/2.71eb3844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16d0a0ac2b5969b1105",
+    "url": "/covid-stock-tracker/static/js/main.16b73a82.chunk.js"
+  },
+  {
+    "revision": "0c74fb99384d3f7cd6b6",
+    "url": "/covid-stock-tracker/static/js/runtime-main.5e44f46c.js"
+  }
+]);
